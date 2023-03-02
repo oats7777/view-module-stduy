1c8b9f476e3fe806ceac88f52eb21a15202da38b
kyu <kyu@spoonradio.co>
2023-03-02 17:26:45 +0900

    하위 컴포넌트 추출을 통한 뷰 분할

--- client/src/Payment.tsx
+++ client/src/Payment.tsx
@@ -41,25 +41,34 @@ const usePaymentMethods = () => {
   };
 };
 
+const PaymentMethods = ({
+  paymentMethods,
+}: {
+  paymentMethods: LocalPaymentMethod[];
+}) => {
+  return (
+    <div>
+      {paymentMethods.map((method) => (
+        <label key={method.provider}>
+          <input
+            type="radio"
+            name="payment"
+            value={method.provider}
+            defaultChecked={method.provider === 'cash'}
+          />
+          <span>{method.label}</span>
+        </label>
+      ))}
+    </div>
+  );
+};
+
 export const Payment = ({ amount }: { amount: number }) => {
   const { paymentMethods } = usePaymentMethods();
-
   return (
     <div>
       <h3>Payment</h3>
-      <div>
-        {paymentMethods.map((method) => (
-          <label key={method.provider}>
-            <input
-              type="radio"
-              name="payment"
-              value={method.provider}
-              defaultChecked={method.provider === 'cash'}
-            />
-            <span>{method.label}</span>
-          </label>
-        ))}
-      </div>
+      <PaymentMethods paymentMethods={paymentMethods} />
       <button>${amount}</button>
     </div>
   );
